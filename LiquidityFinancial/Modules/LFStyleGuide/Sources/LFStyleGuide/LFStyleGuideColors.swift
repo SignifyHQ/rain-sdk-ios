d5c5781080b882781876bca27f1e03b5a740dda9
minhnguyen-c0x12c <minh.nguyen@c0x12c.com>
2023-07-14 17:45:35 +0700

    config: Color and font

--- LiquidityFinancial/Modules/LFStyleGuide/Sources/LFStyleGuide/LFStyleGuideColors.swift
+++ LiquidityFinancial/Modules/LFStyleGuide/Sources/LFStyleGuide/LFStyleGuideColors.swift
@@ -4,7 +4,47 @@ import SwiftUI
 public class LFStyleGuideColors: LFStyleGuide {
   
   public static var backgroundColor: Color {
-    appType == .avalanche ? Colors.AvalancheColors.avalancheDefaultColor.swiftUIColor : Colors.CardanoColors.cardanoDefaultColor.swiftUIColor
+    switch appType {
+      case .avalanche:
+        return Colors.AvalancheColors.background.swiftUIColor
+      case .cardano:
+        return Colors.CardanoColors.background.swiftUIColor
+    }
   }
   
+  public static var secondaryBackgroundColor: Color {
+    switch appType {
+      case .avalanche:
+        return Colors.AvalancheColors.secondaryBackground.swiftUIColor
+      case .cardano:
+        return Colors.CardanoColors.secondaryBackground.swiftUIColor
+    }
+  }
+  
+  public static var primaryColor: Color {
+    switch appType {
+      case .avalanche:
+        return Colors.AvalancheColors.primary.swiftUIColor
+      case .cardano:
+        return Colors.CardanoColors.primary.swiftUIColor
+    }
+  }
+  
+  public static var buttonsColor: Color {
+    switch appType {
+      case .avalanche:
+        return Colors.AvalancheColors.buttons.swiftUIColor
+      case .cardano:
+        return Colors.CardanoColors.buttons.swiftUIColor
+    }
+  }
+  
+  public static var pendingColor: Color {
+    switch appType {
+      case .avalanche:
+        return Colors.AvalancheColors.pending.swiftUIColor
+      case .cardano:
+        return Colors.CardanoColors.pending.swiftUIColor
+    }
+  }
 }
