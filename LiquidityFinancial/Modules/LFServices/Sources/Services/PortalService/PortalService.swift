73d8fafbabd9584da8ea515eb4bd68c707b35c87
spartan-volodymyrdavydenko <volodymyr@c0x12c.com>
2025-04-10 13:53:31 +0300

    Update Portal

--- LiquidityFinancial/Modules/LFServices/Sources/Services/PortalService/PortalService.swift
+++ LiquidityFinancial/Modules/LFServices/Sources/Services/PortalService/PortalService.swift
@@ -400,7 +400,7 @@ private extension PortalService {
       log.error("Portal Swift: Error building transaction parameters for withdrawal. Could not parse expiration to UNIX timestamp")
       throw LFPortalError.unexpected
     }
-
+    
     let adminSignature = try await getAdminSignature(
       addresses: addresses,
       walletAddress: walletAddress,
@@ -568,7 +568,9 @@ private extension PortalService {
       nonce: nonce
     )
     
+    print("ETH SIGN TYPED STARTING...")
     let response = try await portal.request("eip155:\(chainId)", withMethod: .eth_signTypedData_v4, andParams: [walletAddress, messageToSign])
+    print("ETH SIGN COMPLETE")
     
     guard let signatureString = (response.result as? String),
           let signatureData = Data(hexString: signatureString, length: 65)
